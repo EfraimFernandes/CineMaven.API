27d86530d89d66558ff67069d24ce180cbee56d5
Efraim Fernandes <efraim.fernandes@outlook.com>
2025-08-22 02:09:15 -0300

    Model e DTO de Filmes

--- DTOs/FilmeDTO.cs
+++ DTOs/FilmeDTO.cs
@@ -2,5 +2,10 @@
 {
     public class FilmeDTO
     {
+        public string titulo {  get; set; } = string.Empty;
+        public string sinopse {  get; set; } = string.Empty;
+        public int idCategoria { get; set; } = 0;
+        public int anoLancamento { get; set; } = 0;
+        public IFormFile? Capa {  get; set; }
     }
 }
--- Models/Filme.cs
+++ Models/Filme.cs
@@ -2,5 +2,13 @@
 {
     public class Filme
     {
+        public string titulo { get; set; } = string.Empty;
+        public string sinopse { get; set; } = string.Empty;
+        public int idCategoria { get; set; } = 0;
+        public int anoLancamento { get; set; } = 0;
+        public string CapaUrl { get; set; } = string.Empty;
+        public DateTime? dataAlta { get; set; };
+        public string usuarioAlta {  get; set; } = string.Empty;
+
     }
 }
